348cd2fa0af1fe044c91c02ee49fbd42a36aadbd
Juan Felipe <juan9505@gmail.com>
2018-09-05 22:46:29 -0500

    Delete cors options and leave the default configuration

--- server/server.js
+++ server/server.js
@@ -1,28 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const findTollCollectors = require('./findTollCollectors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const findTollCollectors = require("./findTollCollectors");
 
 // CORS Express middleware to enable CORS Requests.
-const cors = require('cors');
-
-const corsOptions = {
-  origin: '*',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}
+const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.json({
-  limit: '50mb'
-}));
+app.use(
+  bodyParser.json({
+    limit: "50mb"
+  })
+);
 
-app.options('*', cors());
+app.options("*", cors());
 
-app.use(cors())
+app.use(cors());
 
-app.post('/findTollCollectors', findTollCollectors);
+app.post("/findTollCollectors", findTollCollectors);
 
 app.listen(1337, () => {
-  console.log('Started on port 1337');
+  console.log("Started on port 1337");
 });
