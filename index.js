86a3e0ba6a128192830258fa9c49428cd45903a7
Juan Felipe <juan9505@gmail.com>
2019-01-01 14:25:06 -0500

    Some env configurations for production mode

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const app = require('./app');
 const port = process.env.PORT;
 
 app.listen(port, '0.0.0.0');
-console.log(`listening on http://localhost:${port}`);
+console.log(
+  `listening on http://localhost:${port} in ${process.env.NODE_ENV} mode`,
+);
