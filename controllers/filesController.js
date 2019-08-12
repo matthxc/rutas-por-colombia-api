0a3b4679940b5e323f8e13ee935f66bb30f55ca9
Juan Felipe Camargo <juan9505@gmail.com>
2019-08-11 20:16:43 -0500

    feat: update dependencies, some improvements and refactor

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,35 +1,38 @@
-const express = require('express');
-const httpStatus = require('http-status');
-const boom = require('boom');
-const asyncMiddleware = require('../middleware/async');
-const { authenticateAdmin } = require('../middleware/authenticate');
-const fileHelper = require('../utils/file');
+const express = require("express");
+const httpStatus = require("http-status");
+const boom = require("boom");
+const asyncMiddleware = require("../middleware/async");
+const { authenticateAdmin } = require("../middleware/authenticate");
+const fileHelper = require("../utils/file");
 
 const router = express.Router();
 
-router.post(
-  '/image',
+router.put(
+  "/image",
   authenticateAdmin,
   asyncMiddleware(async (req, res, next) => {
     const image = req.file;
     if (!image) return next(boom.badRequest());
     return res.status(httpStatus.OK).json({ key: req.key, path: image.path });
-  }),
+  })
 );
 
-router.post(
-  '/image/delete',
+router.delete(
+  "/image/delete",
   authenticateAdmin,
   asyncMiddleware(async (req, res, next) => {
-    const { path } = req.body;
+    const { path } = req.query;
     if (!path) return next(boom.badRequest());
     try {
       await fileHelper.deleteFile(path);
-      return res.status(httpStatus.OK).json('Image deleted');
+      return res.status(httpStatus.OK).json("Image deleted");
     } catch (error) {
+      if (error.code && error.code === "ENOENT") {
+        return next(boom.notFound(error));
+      }
       return next(boom.badRequest(error));
     }
-  }),
+  })
 );
 
 module.exports = router;
